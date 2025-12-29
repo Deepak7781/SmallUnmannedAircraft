778d38f9c0f957c27fb831b928069ea92a6ecba4
Deepak7781 <deepak21082005@gmail.com>
2025-12-29 21:19:24 +0530

    add rotate.m

---
+++ Chapter2/rotate.m
@@ -0,0 +1,13 @@
+function XYZ = rotate(XYZ, phi, theta, psi)
+
+R_roll = [1 0 0; 0 cos(phi) -sin(phi); 0 sin(phi) cos(phi)];
+
+R_theta = [cos(theta) 0 sin(theta); 0 1 0; -sin(theta) 0 cos(theta)];
+
+R_psi = [cos(psi) -sin(psi) 0; sin(psi) cos(psi) 0; 0 0 1];
+
+R = R_roll*R_theta*R_psi;
+
+XYZ = R * XYZ;
+
+end
